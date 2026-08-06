c56331703fdb5a4439b48f6716a78d39b97afebc
Colin Nguyen <nguyen_c9@denison.edu>
2026-08-06 15:51:13 -0400

    Add to prime.h and prime.c

---
+++ src/prime.c
@@ -0,0 +1,4 @@
+#include <math.h>
+#include "prime.h"
+
+// Return whether x is prime or not
---
+++ src/prime.h
@@ -0,0 +1,6 @@
+// This function helps us find the next largest prime number,
+// which is useful for helping us resize hash tables should they
+// get too full or if they're too empty
+int is_prime(const int x);
+int next_prime(int x);
+
